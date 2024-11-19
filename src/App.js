4ac114337cf38019522667fefe66f04ebe6a9288
krrao999 <krkoteswararao999@gmail.com>
2024-11-19 12:06:26 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import {useState} from 'react';
 
 function App() {
+  const [count,setCount] = useState(0);
+
+  const addHandler = ()=>{
+    setCount(count+1);
+}
+const decrement = ()=>{
+    setCount(count-1);
+}
+const doubleHandler = ()=>{
+    setCount((prev)=>prev*2);
+}
+const resetHandler = ()=>{
+    setCount(0);
+}
   return (
+    <div className="container">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+            <h1 style={{textAlign:"center"}} >Counter</h1>
+            <p style={{textAlign:"center"}}>{`Count:${count}`}</p>
+            <div id="buttons">
+            <button className="btn btn-success"onClick={addHandler}>Increment</button>
+            <button className="btn btn-info" style={{color:"white"}} onClick={decrement}>Decrement</button>
+            <button  className="btn btn-primary" onClick={doubleHandler}>double</button>
+            <button className="btn btn-danger" onClick={resetHandler}>Reset</button>
+            </div>
+            </div>
+    </div>
     </div>
   );
 }
